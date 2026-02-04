07b11b027e6a0b027f2ea07aff83e46cc4cb0719
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-02-04 21:35:48 +0600

    made a word frequenct counter function and solved the given problem

--- task_1.js
+++ task_1.js
@@ -1,12 +1,11 @@
 //* Problem 1: Word Frequency Counter
 //? Function name: wordFrequency(sentence)
 //? Statement: Count how many times each word appears in a sentence and return the result as an object.
+
 //? Rules:
 //? Ignore case differences
 
-
 //? Ignore extra spaces
-
 
 //? Test case 1
 //? Input: "I love JS and I love coding and JS is fun"
@@ -17,3 +16,22 @@
 //? Output: { hello: 3 }
 
 // solution
+
+function wordFrequency(sentence) {
+    const words = sentence.trim().split(" ");
+    let obj = {};
+    for(const word of words) {
+        const lowerCaseWord = word.toLowerCase();
+        if(obj.hasOwnProperty(lowerCaseWord)){
+            obj[lowerCaseWord]+=1;
+        }
+        else{
+            obj[lowerCaseWord] = 1;
+        }
+    }
+    return obj;
+}
+
+// const result = wordFrequency("I love JS and I love coding and JS is fun");
+const result = wordFrequency(" Hello hello HELLO ");
+console.log(result);
